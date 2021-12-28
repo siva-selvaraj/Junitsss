8d61aefe48b74f1d608abce99fb2a80c79a3d818
siva-selvaraj <sivashankaranselvaraj@gmail.com>
2021-12-28 17:33:25 +0530

    siva's code

---
+++ src/test/java/org/test/MobileStore.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class MobileStore {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("test 1");
+		System.out.println("test 2");
+
+		System.out.println("test 11");
+		System.out.println("test 22");
+
+		System.out.println("test 111");
+		System.out.println("test 222");
+		
+	}
+
+}
